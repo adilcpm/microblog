94aa431f4671ab2b320278f308bb21182f128ba7
Adil <adil@mcubeh.com>
2020-04-13 09:42:53 +0530

    Chapter 4 | Login & Register

--- app/models.py
+++ app/models.py
@@ -1,15 +1,27 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def  __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
