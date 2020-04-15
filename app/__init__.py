fcbb7362e56363268b93de39580c48f827b6b2f0
Adil <adil@mcubeh.com>
2020-04-15 18:23:26 +0530

    time fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes,models,errors
 
