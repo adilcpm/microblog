52f26a8869cb2da7c1ff2508727e76074210408d
Adil <adil@mcubeh.com>
2020-04-25 20:27:04 +0530

    small fix

--- app/api/posts.py
+++ app/api/posts.py
@@ -17,7 +17,7 @@ def get_post(id):
 def get_posts():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = Post.to_collection_dict(Post.query, page, per_page, 'api.get_posts')
+    data = Post.to_collection_dict(Post.query.order_by(Post.timestamp.desc()), page, per_page, 'api.get_posts')
     return jsonify(data)
 
 @bp.route('/posts', methods=['POST'])
